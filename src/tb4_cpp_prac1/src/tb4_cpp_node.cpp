b5060bd6124108ad0433a53b992712bbf2220e9e
robotai <robotai@V-2204-HPLC-01.staff.ad.curtin.edu.au>
2025-08-07 07:57:18 +0800

    first commit

---
+++ src/tb4_cpp_prac1/src/tb4_cpp_node.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "irobot_create_msgs/msg/interface_buttons.hpp"
+#include "irobot_create_msgs/msg/lightring_leds.hpp"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world tb4_cpp_prac1 package\n");
+  return 0;
+}

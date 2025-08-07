1900d375b227bbf9cd99283bc6212b5738c28df6
robotai <robotai@V-2204-HPLC-01.staff.ad.curtin.edu.au>
2025-08-07 08:15:55 +0800

    lab1_4.2

--- src/tb4_cpp_prac1/src/tb4_cpp_node.cpp
+++ src/tb4_cpp_prac1/src/tb4_cpp_node.cpp
@@ -17,3 +17,19 @@ int main(int argc, char ** argv)
   printf("hello world tb4_cpp_prac1 package\n");
   return 0;
 }
+
+class TurtleBot4Prac1 : public rclcpp::Node
+{
+public:
+  TurtleBot4Prac1(): Node("tb4_prac1") //public constructor names the node as "tb4_prac1"
+  {}
+};
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv); // initialise ROS 2
+  rclcpp::spin(std::make_shared<TurtleBot4Prac1>()); // starts processing data from the node, include subscriber callback
+  rclcpp::shutdown();
+  return 0;
+}

4a5e3e1dc7750da33c9b6a5ea89fd6804e5c166b
robotai <robotai@V-2204-HPLC-01.staff.ad.curtin.edu.au>
2025-08-07 11:13:26 +0800

    save lab 4.6

--- src/tb4_cpp_prac1/src/tb4_cpp_node.cpp
+++ src/tb4_cpp_prac1/src/tb4_cpp_node.cpp
@@ -21,7 +21,7 @@
 class TurtleBot4Prac1 : public rclcpp::Node
 {
 public:
-  TurtleBot4Prac1(): Node("tb4_prac1") //public constructor names the node as "tb4_prac1"
+  TurtleBot4Prac1(): Node("tb4_prac1"), lights_on_(false)//public constructor names the node as "tb4_prac1"
   {
     // Subscribe to the /interface_buttons topic
     interface_buttons_subscriber_ =
@@ -29,8 +29,19 @@ public:
     "/interface_buttons",
     rclcpp::SensorDataQoS(),
     std::bind(&TurtleBot4Prac1::interface_buttons_callback, this, std::placeholders::_1));
+
+    // Create a publisher for the /cmd_lightring topic
+    lightring_publisher_ = this->create_publisher<irobot_create_msgs::msg::LightringLeds>(
+    "/cmd_lightring",
+    rclcpp::SensorDataQoS());
+
+
+
   }
 private:
+
+  bool lights_on_;
+
   // Interface buttons subscription callback
   void interface_buttons_callback(
     const irobot_create_msgs::msg::InterfaceButtons::SharedPtr create3_buttons_msg) // Button 1 is pressed
@@ -38,15 +49,76 @@ private:
     if(create3_buttons_msg->button_1.is_pressed)
     {
       RCLCPP_INFO(this->get_logger(), "Button 1 Pressed!!!");
+
+      button_1_function();
+
     }
   }
-  // Interface Button Subscriber
-  rclcpp::Subscription<irobot_create_msgs::msg::InterfaceButtons>::SharedPtr interface_buttons_subscriber_;
-  // Lightring Publisher
-  rclcpp::Publisher<irobot_create_msgs::msg::LightringLeds>::SharedPtr lightring_publisher_;
 
+    // Perform this function when Button 1 is pressed.
+  void button_1_function()
+  {
+    // Create a ROS 2 message
+    auto lightring_msg = irobot_create_msgs::msg::LightringLeds();
+    // Stamp the message with the current time
+    lightring_msg.header.stamp = this->get_clock()->now();
+
+    if (!lights_on_) 
+    {
+      // Override system lights
+      lightring_msg.override_system = true;
+
+      // Override system lights
+      lightring_msg.override_system = true;
+      // LED 0
+      lightring_msg.leds[0].red = 255;
+      lightring_msg.leds[0].blue = 0;
+      lightring_msg.leds[0].green = 0;
+      // LED 1
+      lightring_msg.leds[1].red = 0;
+      lightring_msg.leds[1].blue = 255;
+      lightring_msg.leds[1].green = 0;
+      // LED 2
+      lightring_msg.leds[2].red = 0;
+      lightring_msg.leds[2].blue = 0;
+      lightring_msg.leds[2].green = 255;
+      // LED 3
+      lightring_msg.leds[3].red = 255;
+      lightring_msg.leds[3].blue = 255;
+      lightring_msg.leds[3].green = 0;
+      // LED 4
+      lightring_msg.leds[4].red = 255;
+      lightring_msg.leds[4].blue = 0;
+      lightring_msg.leds[4].green = 255;
+      // LED 5
+      lightring_msg.leds[5].red = 0;
+      lightring_msg.leds[5].blue = 255;
+      lightring_msg.leds[5].green = 255;
+      
+    }
+    else
+    {
+      // Disable system override. The system will take back control of the lightring.
+      lightring_msg.override_system = false;
+    }
+  
+      
+      
+    //  Publish the message
+    lightring_publisher_->publish(lightring_msg);
+    // Toggle the lights on status
+    lights_on_ = !lights_on_;
+
+
+    }
+
+    
+    // Interface Button Subscriber
+    rclcpp::Subscription<irobot_create_msgs::msg::InterfaceButtons>::SharedPtr interface_buttons_subscriber_;
+    // Lightring Publisher
+    rclcpp::Publisher<irobot_create_msgs::msg::LightringLeds>::SharedPtr lightring_publisher_;
+  
 };
-
 
 
 int main(int argc, char * argv[])

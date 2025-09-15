9f3f8933bf5e43f2160c2293ce7fff5020bf9561
robotai <robotai@V-2204-HPLC-05.staff.ad.curtin.edu.au>
2025-09-15 11:27:03 +0800

    1.4 done not tested

--- src/tb4_cpp_prac5/src/likelihood_field_model.cpp
+++ src/tb4_cpp_prac5/src/likelihood_field_model.cpp
@@ -5,6 +5,7 @@
 
 namespace iar_amcl
 {
+    /// class constructor
     LikelihoodFieldModel::LikelihoodFieldModel(
     double z_hit, double z_max, double z_rand, double sigma_hit,
     double max_occ_dist, size_t max_beams, map_t * map)
@@ -14,7 +15,7 @@ namespace iar_amcl
         z_rand_ = z_rand;
         sigma_hit_ = sigma_hit;
         z_max_ = z_max;
-        map_update_cspace(map, max_occ_dist);
+        map_update_cspace(map, max_occ_dist); /// LINE 2 of algorithm 1. this is the pre-computation that computes the likelihood field map.
     }
 
     // Determine the probability for the given pose
@@ -46,7 +47,27 @@ namespace iar_amcl
                 /* TODO TASK - MILSTONE 1.1 
                     Compute the pose of the lidar sensor.
                 */
-     
+
+                pf_vector_t lidar_pose_to_robot = self->laser_pose_; /// Tsens = {xT, yT, thetaT} = sensor in robot's frame
+
+                const double xr = pose_robot.v[0];
+                const double yr = pose_robot.v[1];
+                const double thetar = pose_robot.v[2];
+
+                const double xT = lidar_pose_to_robot.v[0];
+                const double yT = lidar_pose_to_robot.v[1];
+                const double thetaT = lidar_pose_to_robot.v[2];
+
+                const double xs;
+                const double ys;
+                const double thetas;
+
+                xs = xr + xT * cos(thetar) - yT * sin(thetar);
+                ys = yr + xT * cos(thetar) + yT * sin(thetar);
+                thetas = thetar + thetaT;
+
+            
+
 
                 p = 1.0;
 
@@ -65,11 +86,25 @@ namespace iar_amcl
                         Check whether the laser beam's value is infinity, if yes, skip this beam measurement
                     */
 
+                    if (isinf(obs_range)) {
+                        continue;
+                    }
+
+                    if (isnan(obs_range)) {
+                        continue;
+                    }
+
+
 
                     /* 
                         TODO TASK - MILESTONE 1.3
                         Check whether a failure measurement is detected, i.e., max range is detected. If yes, update the probability
                     */
+                    if (obs_range == data->range_max) {
+                        pz = pz + self->z_max_
+                    }
+
+
 
 
                     /* 
@@ -77,7 +112,42 @@ namespace iar_amcl
                         Process a beam with range measurement less than the maximum value.
                     */
 
-                    
+                    if (obs_range < data->range_max) {
+
+                        pz = pz + ( self->z_rand_ * (1 / data->range_max ));
+
+                        const double xhit;
+                        const double yhit;
+
+                        xhit = xs + obs_range * cos(thetas + obs_bearing);
+                        yhit = ys + obs_range * sin(thetas + obs_bearing);
+
+                        int m_x = MAP_GXWX(self->map_, xhit); /// convert from global frame to map frame
+                        int m_y = MAP_GYWY(self->map_, yhit); ///
+
+                        bool valid = MAP_VALID(self->map_, m_x, m_y); /// check if point lies within map boundary
+
+                        if (valid) {
+                            
+                            int index = MAP_INDEX(self->map_, m_x, m_y); /// get the map cell's index number
+                            dist = self->map_->cells[index].occ_dist; /// get the map cell's distance to nearest obstacle.
+                            
+
+
+                        }
+                        else {
+                            dist = self->map_->max_occ_dist;
+                        }
+                        
+
+                        pz = pz + self->z_hit_ * exp(-(dist * dist) / (2.0 * self->sigma_hit_ * self->sigma_hit_) )
+
+
+
+
+                    }
+
+
                     
                     assert(pz <= 1.0);
                     assert(pz >= 0.0);
